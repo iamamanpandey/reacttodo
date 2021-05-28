e0666852371f8349cf750f0c9245dc7fc2dba22d
iamamanpandey <iamamanpandey007@gmail.com>
2021-05-28 12:57:00 +0530

    first todo list

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./List";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Nav />
+      <List />
+    </div>
+  );
+}
+export default App;
