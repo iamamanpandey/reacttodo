4a8bfe08f6908733c0799d719309772683fb2e4f
iamamanpandey <iamamanpandey007@gmail.com>
2021-05-28 12:59:20 +0530

    fix lines

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Nav from "./Nav";
 function App() {
   return (
     <div className="App">
-      
       <Nav />
       <List />
     </div>
--- src/Nav.js
+++ src/Nav.js
@@ -8,8 +8,7 @@ function Nav() {
           Navbar
         </a>
         <ul class="nav">
-          <li class="nav-item">
-          </li>
+          <li class="nav-item"></li>
         </ul>
       </div>
     </nav>
