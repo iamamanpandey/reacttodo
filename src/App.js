7e26d9607a858288b2f6a7dd4a71c9179a3cfc88
iamamanpandey <iamamanpandey007@gmail.com>
2021-06-02 15:13:39 +0530

    remove unwanted npm package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import TodoList from "./TodoList";
-import {Tabs, Tab} from 'react-bootstrap';
 import TodoForm from "./TodoForm";
 import "./App.css";
 
@@ -13,9 +12,9 @@ function App() {
   ]);
 
   const [value, setvalue] = useState("All");
-  const [key, setKey] = useState('1');
   const [filtertodos, setfilterTodos] = useState([...todos]);
 
+  //for items left
   var trueCount = 0;
   var falseCount = 0;
   filtertodos.forEach((i) => {
@@ -27,7 +26,7 @@ function App() {
     setfilterTodos([...todos, { task, isCompleted: false }]);
   };
 
-  const handlefilterChange = (e) => {
+const handlefilterChange = (e) => {
     console.log(e.target.value)
     const filtertodo = todos.filter((todo) => {
       switch (e.target.value) {
@@ -72,17 +71,15 @@ function App() {
             fontSize: "100px",
             fontWeight: "400",
           }}
-        >
-          todos
-        </h1>
+        > todos
+   </h1>
       </div>
       <div
         class="card mx-auto shadow-lg"
         style={{ width: "30%", backgroundColor: "#ffff" }}
-      >
-        
+        >
           <TodoForm addTodo={addTodo} />
-          <ul class="list-group list-group-flush">
+           <ul class="list-group list-group-flush">
             {filtertodos.map((todo, id) => (
               <TodoList
                 key={id}
@@ -95,32 +92,20 @@ function App() {
           </ul>
           <div className="d-flex my-2 justify-content-between ">
             <p className="my-3" style={{   marginLeft: "3%",  fontSize: "14px",  color: "#4d4d4d",  fontFamily: "sans-serif",}}>
-              {falseCount} items left </p>
-           
-            
+              {falseCount} items left </p>  
               <div class="my-2" onClick={handlefilterChange}>  
-                <button  value="All"       className={value == 'All' ? 'active' : 'btnn'}       onClick={() => setvalue('All')}> All </button>
-                <button  value="Active"    className={value == 'Active' ? 'active' : 'btnn'}    onClick={() => setvalue('Active')}>  Active </button>
-                <button  value="Completed" className={value == 'Completed' ? 'active' : 'btnn'} onClick={() => setvalue('Completed')} > Completed </button>
+                <button  value="All"       className={value === 'All' ? 'active' : 'btnn'}       onClick={() => setvalue('All')}> All </button>
+                <button  value="Active"    className={value === 'Active' ? 'active' : 'btnn'}    onClick={() => setvalue('Active')}>  Active </button>
+                <button  value="Completed" className={value === 'Completed' ? 'active' : 'btnn'} onClick={() => setvalue('Completed')} > Completed </button>
               </div>
-           
 
-            <a class="nav-item nav-link btn1" style={{
-             marginLeft: "1%",
-                marginRight: "3%",
-                marginTop: "1%",
-                fontSize: "14px",
-                color: "#4d4d4d",
-                fontFamily: "sans-serif",
-              }}
+            <a class="nav-item nav-link btn1" style={{ marginLeft: "1%",marginRight: "3%", marginTop: "1%", fontSize: "14px", color: "#4d4d4d", fontFamily: "sans-serif",}}
               onClick={clearCompletedTodo}
-            >
-              Clear completed
+             > Clear completed
             </a> 
           </div>
         </div>  
       </div>
-  
   );
 }
 export default App;
