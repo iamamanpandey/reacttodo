55894ae6da2378b90584736a088d5b8ddc5b3d9e
iamamanpandey <iamamanpandey007@gmail.com>
2021-06-02 14:26:46 +0530

    fixed  tab design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import TodoList from "./TodoList";
-import {Tab,Tabs} from 'react-bootstrap';
+import {Tabs, Tab} from 'react-bootstrap';
 import TodoForm from "./TodoForm";
 import "./App.css";
 
@@ -12,8 +12,8 @@ function App() {
     },
   ]);
 
-
-  const [value, setvalue] = useState();
+  const [value, setvalue] = useState("All");
+  const [key, setKey] = useState('1');
   const [filtertodos, setfilterTodos] = useState([...todos]);
 
   var trueCount = 0;
@@ -32,13 +32,13 @@ function App() {
     const filtertodo = todos.filter((todo) => {
       switch (e.target.value) {
         case "All":
-          return todo ;
+          return todo;
         case "Completed":
           return todo.isCompleted === true;
         case "Active":
           return todo.isCompleted === false;
         default:
-          return todo;
+          return todo.length;
       }
     });
     setfilterTodos(filtertodo);
@@ -62,10 +62,6 @@ function App() {
     setfilterTodos(newTodos);
   };
 
-  const handleTabs =(e)=>{
- console.log(e.target.value)
-  } 
-
   return (
     <div className="App mx-auto">
       <div className="text-center " style={{ marginTop: "3%" }}>
@@ -76,14 +72,15 @@ function App() {
             fontSize: "100px",
             fontWeight: "400",
           }}
-        >todos
+        >
+          todos
         </h1>
       </div>
       <div
         class="card mx-auto shadow-lg"
         style={{ width: "30%", backgroundColor: "#ffff" }}
       >
-        <div>
+        
           <TodoForm addTodo={addTodo} />
           <ul class="list-group list-group-flush">
             {filtertodos.map((todo, id) => (
@@ -96,44 +93,34 @@ function App() {
               />
             ))}
           </ul>
-          <div className="d-flex my-2 justify-content-between border-top">
-            <p
-              className="my-3"
-              style={{
-                marginLeft: "3%",
-                fontSize: "16px",
+          <div className="d-flex my-2 justify-content-between ">
+            <p className="my-3" style={{   marginLeft: "3%",  fontSize: "14px",  color: "#4d4d4d",  fontFamily: "sans-serif",}}>
+              {falseCount} items left </p>
+           
+            
+              <div class="my-2" onClick={handlefilterChange}>  
+                <button  value="All"       className={value == 'All' ? 'active' : 'btnn'}       onClick={() => setvalue('All')}> All </button>
+                <button  value="Active"    className={value == 'Active' ? 'active' : 'btnn'}    onClick={() => setvalue('Active')}>  Active </button>
+                <button  value="Completed" className={value == 'Completed' ? 'active' : 'btnn'} onClick={() => setvalue('Completed')} > Completed </button>
+              </div>
+           
+
+            <a class="nav-item nav-link btn1" style={{
+             marginLeft: "1%",
+                marginRight: "3%",
+                marginTop: "1%",
+                fontSize: "14px",
                 color: "#4d4d4d",
                 fontFamily: "sans-serif",
               }}
+              onClick={clearCompletedTodo}
             >
-              {falseCount} items left
-            </p>
-            <div
-              class="btn-group btn-group-toggle "
-              data-toggle="buttons"
-              onClick={handlefilterChange}
-              value={value}
-            >
-            <div class="my-2"  aria-label="Basic example" onClick={handlefilterChange}  value={value}>
-             <button type="button" class="btnn" value="All" tabindex="1" activeClassName="active">All</button>
-             <button type="button" class=" btnn  mx-1" value="Active" tabindex="2">Active</button>
-             <button type="button" class=" btnn  "  value="Completed" tabindex="3">Completed</button>
-            </div>
-      
-            </div>
-          
-            <a class="nav-item nav-link btn1" style={{
-                marginLeft: "1%",
-                marginRight:'3%',
-                marginTop:'2%',
-                fontSize: "16px",
-                color: "#4d4d4d",
-                fontFamily: "sans-serif",
-              }}   onClick={clearCompletedTodo}>Clear completed</a>
+              Clear completed
+            </a> 
           </div>
-        </div>
+        </div>  
       </div>
-    </div>
+  
   );
 }
 export default App;
